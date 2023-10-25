21a658bf02ade766cf06deead39afac208069f0d
Ilham Triza Kurniawan <95692045+ilhamtrz@users.noreply.github.com>
2023-10-25 19:50:08 +0700

    Add basic enemy movement

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 const speed = 200.0
 
-func _process(_delta):
+func _physics_process(_delta):
 	
 	# move input
 	var direction = Input.get_vector("player_left","player_right","player_up","player_down")
