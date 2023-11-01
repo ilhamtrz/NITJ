57bf389a08e026a6ced99160c49869193ea6cd8a
Ilham Triza Kurniawan <95692045+ilhamtrz@users.noreply.github.com>
2023-11-01 08:03:07 +0700

    add player animation

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,10 +1,29 @@
 extends CharacterBody2D
 
 const speed = 200.0
+@onready var sprite = $PlayerImage
+@onready var walkTimer = %walkTimer
 
 func _physics_process(_delta):
+	movement()
 	
+
+func movement():
 	# move input
 	var direction = Input.get_vector("player_left","player_right","player_up","player_down")
+	
+	if direction.x < 0:
+		sprite.flip_h = true
+	elif direction.x > 0:
+		sprite.flip_h = false
+	
+	if direction != Vector2.ZERO:
+		if walkTimer.is_stopped():
+			if sprite.frame >= sprite.hframes - 1:
+				sprite.frame = 0
+			else:
+				sprite.frame += 1
+			walkTimer.start()
+			
 	velocity = direction.normalized() * speed
 	move_and_slide()
