6e72ddcbe342899ff0684deeee742b7c4301e275
Ilham Triza Kurniawan <95692045+ilhamtrz@users.noreply.github.com>
2023-12-05 22:03:10 +0700

    add tornado weapon

--- Scenes/Player/Attack/tornado.gd
+++ Scenes/Player/Attack/tornado.gd
@@ -2,10 +2,10 @@ extends Area2D
 
 var level = 1
 var hp = 9999
-var speed = 100.0
+var speed = 400
 var damage = 5
 var knockback_amount = 100
-var attack_size = 1.0
+var attack_size = 1.5
 
 var last_movement = Vector2.ZERO
 var angle = Vector2.ZERO
@@ -20,12 +20,59 @@ func _ready():
 	match level:
 		1:
 			hp = 9999
-			speed = 100.0
+			speed = 400
 			damage = 5
 			knockback_amount = 100
-			attack_size = 1.0 
+			attack_size = 1.5
 	
-	#var tween = create_tween()
-	#tween.tween_property(self, "scale",Vector2(1,1)*attack_size,1).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
-	#tween.play()
+	var move_to_less = Vector2.ZERO
+	var move_to_more = Vector2.ZERO
+	
+	match last_movement:
+		Vector2.UP, Vector2.DOWN:
+			move_to_less = global_position + Vector2(randf_range(-1,-0.25),last_movement.y)*500
+			move_to_more = global_position + Vector2(randf_range(0.25,1),last_movement.y)*500
+		Vector2.RIGHT, Vector2.RIGHT:
+			move_to_less = global_position + Vector2(last_movement.x, randf_range(-1,-0.25))*500
+			move_to_more = global_position + Vector2(last_movement.x, randf_range(0.25,1))*500
+		Vector2(1,1),Vector2(1,-1),Vector2(-1,-1),Vector2(-1,1):
+			move_to_less = global_position + Vector2(last_movement.x, last_movement.y * randf_range(0,0.75))*500
+			move_to_more = global_position + Vector2(last_movement.x * randf_range(0,0.75), last_movement.y)*500
+	
+	angle_less = global_position.direction_to(move_to_less)
+	angle_more = global_position.direction_to(move_to_more)
+	
+	var initital_tween = create_tween().set_parallel(true)
+	initital_tween.tween_property(self,"scale",Vector2(1,1)*attack_size,3).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+	var final_speed = speed
+	speed = speed/5.0
+	initital_tween.tween_property(self,"speed",final_speed,6).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+	initital_tween.play()
+	
+	var tween = create_tween()
+	var set_angle = randi_range(0,1)
+	if set_angle == 1:
+		angle = angle_less
+		tween.tween_property(self,"angle", angle_more,2)
+		tween.tween_property(self,"angle", angle_less,2)
+		tween.tween_property(self,"angle", angle_more,2)
+		tween.tween_property(self,"angle", angle_less,2)
+		tween.tween_property(self,"angle", angle_more,2)
+		tween.tween_property(self,"angle", angle_less,2)
+	else:
+		angle = angle_more
+		tween.tween_property(self,"angle", angle_less,2)
+		tween.tween_property(self,"angle", angle_more,2)
+		tween.tween_property(self,"angle", angle_less,2)
+		tween.tween_property(self,"angle", angle_more,2)
+		tween.tween_property(self,"angle", angle_less,2)
+		tween.tween_property(self,"angle", angle_more,2)
+	tween.play()
+	
+func _physics_process(delta):
+	position += angle*speed*delta
+
+func _on_timer_timeout():
+	emit_signal("remove_from_array",self)
+	queue_free()
 
