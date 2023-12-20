42795659fa6525d983e28a07ecb05c8ac8657867
Ilham Triza Kurniawan <95692045+ilhamtrz@users.noreply.github.com>
2023-12-20 20:54:07 +0700

    enemy drop exp gem implemented

--- Scenes/Enemies/enemy.gd
+++ Scenes/Enemies/enemy.gd
@@ -3,13 +3,16 @@ extends CharacterBody2D
 @export var movement_speed = 50.0
 @export var hp = 20.0
 @export var knockback_recovery = 3.5
+@export var experience = 1
 var knockback = Vector2.ZERO
 
 @onready var player = get_tree().get_first_node_in_group("player")
+@onready var loot_base = get_tree().get_first_node_in_group("loot")
 @onready var sprite = $EnemyImage
 @onready var sound_hit = $SoundHit
 
 var death_anim = preload("res://Scenes/Enemies/explosion.tscn")
+var exp_gem = preload("res://Scenes/Objects/experience_gem.tscn")
 
 signal remove_from_array(object)
 
@@ -32,6 +35,10 @@ func death():
 	enemy_death.scale = sprite.scale
 	enemy_death.global_position = global_position
 	get_parent().call_deferred("add_child",enemy_death)
+	var new_gem = exp_gem.instantiate()
+	new_gem.global_position = global_position
+	new_gem.experience = experience
+	loot_base.call_deferred("add_child",new_gem)
 	queue_free()
 
 func _on_hurt_box_hurt(damage, angle, knockback_amount):
