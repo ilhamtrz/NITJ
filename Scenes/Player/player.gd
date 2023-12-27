d68847776024ee20b540a4dfec133d7cbcb76f09
Ilham Triza Kurniawan <95692045+ilhamtrz@users.noreply.github.com>
2023-12-27 22:18:02 +0700

    Level Up Panel implemented

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -46,6 +46,10 @@ var enemy_close = []
 #GUI
 @onready var expBar = %ExperienceBar
 @onready var lblLevel = %lbl_level
+@onready var levelPanel =  %LevelUp
+@onready var upgradeOptions =  %UpgradeOptions
+@onready var itemOptions = preload("res://Scenes/Utility/item_option.tscn")
+@onready var sndLevelUp = %snd_levelup
 
 func _ready():
 	attack()
@@ -176,11 +180,9 @@ func calculate_experience(gem_exp):
 	if experience + collected_experience >= exp_required: #level up
 		collected_experience -= exp_required - experience
 		experience_level += 1
-		lblLevel.text = str("Level: ",experience_level)
 		experience = 0
 		exp_required = calculate_experiencecap()
-		calculate_experience(0)
-		#levelup()
+		levelup()
 	else:
 		experience += collected_experience
 		collected_experience = 0
@@ -200,6 +202,30 @@ func calculate_experiencecap():
 func set_expbar(set_value = 1, set_max_value = 100):
 	expBar.value = set_value
 	expBar.max_value = set_max_value
+
+func levelup():
+	sndLevelUp.play()
+	lblLevel.text = str("Level: ",experience_level)
+	var tween = levelPanel.create_tween()
+	tween.tween_property(levelPanel,"position",Vector2(440,110),0.2).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_IN)
+	tween.play()
+	levelPanel.visible = true
+	var options = 0
+	var optionsmax = 3
+	while options < optionsmax:
+		var option_choice = itemOptions.instantiate()
+		upgradeOptions.add_child(option_choice)
+		options += 1
+	get_tree().paused = true
+
+func upgrade_character(upgrade):
+	var option_children = upgradeOptions.get_children()
+	for i in option_children:
+		i.queue_free()
+	levelPanel.visible = false
+	levelPanel.position = Vector2(1500,50)
+	get_tree().paused = false
+	calculate_experience(0)
 
 
 
