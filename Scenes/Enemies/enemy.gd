78b4dd98a993882b2a492ac30ade2f1742fb836b
Ilham Triza Kurniawan <95692045+ilhamtrz@users.noreply.github.com>
2024-02-21 20:41:31 +0700

    Add enemy variations scene

--- Scenes/Enemies/enemy.gd
+++ Scenes/Enemies/enemy.gd
@@ -4,17 +4,22 @@ extends CharacterBody2D
 @export var hp = 20.0
 @export var knockback_recovery = 3.5
 @export var experience = 1
+@export var enemy_damage = 1
 var knockback = Vector2.ZERO
 
 @onready var player = get_tree().get_first_node_in_group("player")
 @onready var loot_base = get_tree().get_first_node_in_group("loot")
 @onready var sprite = $EnemyImage
 @onready var sound_hit = $SoundHit
+@onready var hitBox = $HitBox
 
 var death_anim = preload("res://Scenes/Enemies/explosion.tscn")
 var exp_gem = preload("res://Scenes/Objects/experience_gem.tscn")
 
 signal remove_from_array(object)
+
+func _ready():
+	hitBox.damage = enemy_damage
 
 func _physics_process(_delta):
 	knockback = knockback.move_toward(Vector2.ZERO, knockback_recovery)
