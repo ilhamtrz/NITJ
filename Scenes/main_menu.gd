fe888ef14eccce0ac756d5b1b76c7d5e25f10210
Ilham Triza Kurniawan <95692045+ilhamtrz@users.noreply.github.com>
2024-05-31 07:51:23 +0700

    feat: add PRNG to stage selection

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -1,10 +1,17 @@
 extends Control
 
-@export var mainGameScene: PackedScene
+@export var mainGameScenes: Array[PackedScene] = []
+# Create a new instance of RandomNumberGenerator
+var rng = RandomNumberGenerator.new()
+#Set the seed value
+
 
 func _on_play_button_pressed():
-	get_tree().change_scene_to_packed(mainGameScene)
-
+	rng.seed = hash("123")
+	var sceneIndex = rng.randi_range(0, mainGameScenes.size() - 1)
+	print("Stage variation Selected:",sceneIndex)
+	var selectedScene = mainGameScenes[sceneIndex]
+	get_tree().change_scene_to_packed(selectedScene)
 
 func _on_exit_button_pressed():
 	get_tree().quit()
