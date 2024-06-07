7f715c5502cb1cd08c29d491f44386cbd00742c5
Ilham Triza Kurniawan <95692045+ilhamtrz@users.noreply.github.com>
2024-06-07 08:11:30 +0700

    feat: add seed text field to main menu

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -3,6 +3,8 @@ extends Control
 var LCG = preload("res://Scripts/LCG.gd")
 
 @export var mainGameScenes: Array[PackedScene] = []
+
+@onready var seed_text = get_node("MarginContainer/VBoxContainer/LineEdit")
 # Create a new instance of RandomNumberGenerator
 #var rng = RandomNumberGenerator.new()
 #Set the seed value
@@ -11,9 +13,15 @@ var LCG = preload("res://Scripts/LCG.gd")
 #var seed_value = 1
 
 # Create an instance of the LCG class with the seed value
-var lcg_instance = LCG.new(GlobalSettings.seed)
+
+
+func _ready():
+	seed_text.grab_focus()
 
 func _on_play_button_pressed():
+	GlobalSettings.seed = int(seed_text.text)
+	var lcg_instance = LCG.new(GlobalSettings.seed)
+	print("Seed Value: ", GlobalSettings.seed)
 	var sceneIndex = lcg_instance.rand_range(0, mainGameScenes.size() - 1)
 	print("Stage variation Selected:",sceneIndex)
 	var selectedScene = mainGameScenes[sceneIndex]
